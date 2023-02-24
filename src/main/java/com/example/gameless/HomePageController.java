da7866dc53bc5471a80b8391203404267fa84606
Lindsay-X <70488690+Lindsay-X@users.noreply.github.com>
2023-02-24 18:54:38 -0500

    Connecting the current admin pages with buttons
    Admin home page, event page, student info page

---
+++ src/main/java/com/example/gameless/HomePageController.java
@@ -0,0 +1,35 @@
+package com.example.gameless;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HomePageController {
+
+    @FXML
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void studentInfoButtonOnAction(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("admin/AdminStudentInfoPage.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void adminEventButtonOnAction(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("admin/AdminStudentInfoPage.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
--- src/main/java/com/example/gameless/StudentInfoPageController.java
+++ src/main/java/com/example/gameless/StudentInfoPageController.java
@@ -4,13 +4,19 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -66,5 +72,17 @@ public class StudentInfoPageController implements Initializable {
 
     public void getGrade(ActionEvent event){
         String gradeChosen = adminStudentGradeChoice.getValue();
+    }
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void backButtonOnAction(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("admin/AdminHomePage.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
