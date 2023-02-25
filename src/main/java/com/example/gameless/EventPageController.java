136e274d3de44a0f86eab9db72efc73da2d194c9
bduanmu <74875982+bduanmu@users.noreply.github.com>
2023-02-25 15:27:31 -0500

    Added functionality to publish events with db connection

--- src/main/java/com/example/gameless/EventPageController.java
+++ src/main/java/com/example/gameless/EventPageController.java
@@ -1,18 +1,33 @@
 package com.example.gameless;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class EventPageController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    @FXML
+    private TextField eventName;
+    @FXML
+    private TextField eventTime;
+    @FXML
+    private TextField eventLocation;
+    @FXML
+    private TextArea eventDescription;
 
     public void backButtonOnAction(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("admin/AdminHomePage.fxml"));
@@ -23,6 +38,27 @@ public class EventPageController {
     }
 
     public void backEventButtonOnAction(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("admin/AdminEventPage.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void publishButtonOnAction(ActionEvent event) throws IOException {
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDb = connectNow.getConnection();
+
+        String addEvent = "INSERT INTO events (eventName, eventDescription, eventLocation, eventTime) VALUES ('" + eventName.getText() + "', '" + eventDescription.getText() + "', '" + eventLocation.getText() + "', '" + eventTime.getText() + "')";
+
+        try {
+            Statement statement = connectDb.createStatement();
+            statement.executeUpdate(addEvent);
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+
         root = FXMLLoader.load(getClass().getResource("admin/AdminEventPage.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
